43cc9db853d32597fba2966e0ff6c25352bf8a64
Sameh <sameh.abdelhaleem95@gmail.com>
2022-12-28 20:35:48 +0200

    add MainNav component and link it to NewQuote and AllQuotes paths

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import { Redirect, Route, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import QuoteDetail from "./pages/QuoteDetail";
 import Quote from "./pages/QuoteDetail";
 import Quotes from "./pages/Quotes";
 
 function App() {
   return (
-    <Switch>
-      <Route path={"/"} exact>
-        <Redirect to="/quotes" />
-      </Route>
+    <Layout>
+      <Switch>
+        <Route path={"/"} exact>
+          <Redirect to="/quotes" />
+        </Route>
 
-      <Route path={"/quotes"} exact>
-        <Quotes></Quotes>
-      </Route>
-      <Route path={"/quotes/:quoteId"}>
-        <Quote></Quote>
-      </Route>
-      <Route path={"/new-quote"}>
-        <QuoteDetail></QuoteDetail>
-      </Route>
-    </Switch>
+        <Route path={"/quotes"} exact>
+          <Quotes></Quotes>
+        </Route>
+        <Route path={"/quotes/:quoteId"}>
+          <Quote></Quote>
+        </Route>
+        <Route path={"/new-quote"}>
+          <QuoteDetail></QuoteDetail>
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 

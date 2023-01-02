50551df1265615edc5e5cf3e1958ec1996cce28b
Sameh <sameh.abdelhaleem95@gmail.com>
2023-01-02 22:00:13 +0200

    conditinally render load comments button using nested routes

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,4 +1,4 @@
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 const DUMMY_QUOTES = [
@@ -22,7 +22,13 @@ const QuoteDetail = () => {
   return (
     <section>
       <HighlightedQuote text={quote.text} author={quote.author} />
-
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>

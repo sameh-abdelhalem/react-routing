374078e9b5fe01ec7c7225f8050e849b0c33a06e
Sameh <sameh.abdelhaleem95@gmail.com>
2023-01-02 21:40:50 +0200

    extract query params for sorting ascending and descending and change the button title dynamicly

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,27 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
+  const location = useLocation();
+  const history = useHistory();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const changeSortingHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+  };
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
